c0fa7b04e52dcb2ac18d432d0da6485fe124e7ee
Stefan Reusens <stefan.reusens@gmail.com>
2019-05-20 16:59:37 +0200

    klaar voor vandaag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,7 @@ import React, {Component} from 'react'
 class Header extends Component {
     render() {
         return (
-         <h1>Hamburger-Restaurant-Met-De-Veel-Te-Lange-Naam-Voor-Zichzelf-En-Zijn-Gerechten
-               
-            </h1>
+            <h1>Burgertijd</h1>
         )
     }
 }
